168faff41137869c451835c6654c45dc1696770e
gkresic <gordan.kresic@steatoda.com>
2021-01-14 19:06:14 +0100

    Process POSTed JSON in sequential mode
    Avoid building all instances in memory.

--- shark/src/main/java/com/steatoda/muddywaters/shark/Handler.java
+++ shark/src/main/java/com/steatoda/muddywaters/shark/Handler.java
@@ -1,32 +1,40 @@
 package com.steatoda.muddywaters.shark;
 
+import java.util.Iterator;
+
 import org.rapidoid.http.Req;
 import org.rapidoid.http.ReqRespHandler;
 import org.rapidoid.http.Resp;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Handler implements ReqRespHandler {
 
 	@Override
 	public Object execute(Req req, Resp resp) throws Exception {
-	
-		Payload[] payloads = Mapper.readValue(req.body(), Payload[].class);
-		
+
 		Payload max = new Payload();
-		for (Payload payload : payloads) {
+
+		Iterator<Payload> iter = Mapper.readerFor(PayloadRef).readValues(req.body());
+
+		while (iter.hasNext()) {
+			Payload payload = iter.next();
 			if (max.number == null || max.number < payload.number)
 				max.number = payload.number;
 			if (max.text == null || max.text.compareTo(payload.text) < 0)
 				max.text = payload.text;
 		}
 		
-		return resp.code(200).body(Mapper.writeValueAsBytes(max));
+		return resp
+			.code(200)
+			.body(Mapper.writeValueAsBytes(max));
 		
 	}
 
 	private static final long serialVersionUID = 1L;
 
 	private static final ObjectMapper Mapper = new ObjectMapper();
+	private static final TypeReference<Payload> PayloadRef = new TypeReference<Payload>() {};
 	
 }
