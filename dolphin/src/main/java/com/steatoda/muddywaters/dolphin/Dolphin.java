953199e423ba1fd82b0b6132d785fb89403b66dd
gkresic <gordan.kresic@steatoda.com>
2022-10-20 10:09:19 +0200

    Dolphin: extract Vert.x ops into VertxService

--- dolphin/src/main/java/com/steatoda/muddywaters/dolphin/Dolphin.java
+++ dolphin/src/main/java/com/steatoda/muddywaters/dolphin/Dolphin.java
@@ -1,10 +1,6 @@
 package com.steatoda.muddywaters.dolphin;
 
-import com.steatoda.muddywaters.dolphin.vertx.RestVerticle;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.CompositeFuture;
-import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
+import com.steatoda.muddywaters.dolphin.vertx.VertxService;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.NoSubscriberEvent;
 import org.greenrobot.eventbus.Subscribe;
@@ -13,19 +9,17 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Singleton
 public class Dolphin {
 
 	@Inject
-	public Dolphin(EventBus eventBus, Vertx vertx) {
+	@SuppressWarnings("unused")	// extra parameters are injected only to be initialized as soon as possible ("eager singletons")
+	public Dolphin(
+		EventBus eventBus,
+		VertxService vertxService
+	) {
 		this.eventBus = eventBus;
-		this.vertx = vertx;
 		eventBus.register(this);
 	}
 
@@ -42,17 +36,6 @@ public class Dolphin {
 
 		eventBus.post(new DolphinPreStartEvent(this));
 
-		try {
-			awaitComplete(CompositeFuture.all(
-				Stream.of(
-					RestVerticle.deploy(vertx)
-				).peek(future -> future.onSuccess(verticeDeploymentIds::add)).collect(Collectors.toList())
-			));
-		} catch (Exception e) {
-			throw new RuntimeException("Error deploying vertice(s)", e);
-		}
-		Log.info("{} Vert.x vertice(s) deployed", verticeDeploymentIds.size());
-
 		eventBus.post(new DolphinStartEvent(this));
 		
 		Log.info("Dolphin {} is up and runnin'", DolphinProperties.get().getVersion());
@@ -64,16 +47,6 @@ public class Dolphin {
 		Log.debug("Dolphin {} stopping...", DolphinProperties.get().getVersion());
 
 		eventBus.post(new DolphinPreStopEvent(this));
-
-		try {
-			int verticlesCount = verticeDeploymentIds.size();
-			if (verticlesCount > 0)
-				awaitComplete(CompositeFuture.all(verticeDeploymentIds.stream().map(vertx::undeploy).collect(Collectors.toList())));
-			verticeDeploymentIds.clear();
-			Log.info("{} vertices undeployed", verticlesCount);
-		} catch (Exception e) {
-			Log.error("Error undeploying vertices", e);
-		}
 
 		eventBus.post(new DolphinStopEvent(this));
 		
@@ -87,74 +60,14 @@ public class Dolphin {
 
 		eventBus.post(new DolphinPreDestroyEvent(this));
 
-		try {
-			Promise<Void> vertxClosePromise = Promise.promise();
-			vertx.close(result -> {
-				if (result.succeeded())
-					vertxClosePromise.complete(result.result());
-				else
-					vertxClosePromise.fail(result.cause());
-			});
-			awaitComplete(vertxClosePromise.future());
-			Log.info("Vert.x closed");
-		} catch (Exception e) {
-			Log.error("Error closing Vert.x", e);
-		}
-
 		eventBus.post(new DolphinDestroyEvent(this));
 		
 		Log.info("Dolphin {} successfully shut down - bye!", DolphinProperties.get().getVersion());
 		
 	}
 
-	// adopted from https://stackoverflow.com/a/68543780/4553548
-	private static <T> T awaitComplete(io.vertx.core.Future<T> future) throws InterruptedException {
-
-		final Object lock = new Object();
-		final AtomicReference<AsyncResult<T>> resultRef = new AtomicReference<>(null);
-
-		T result;
-		Throwable cause;
-
-		if (future.isComplete()) {
-			result = future.result();
-			cause = future.cause();
-		} else {
-			synchronized (lock) {
-				// We *must* be locked before registering a callback.
-				// If result is ready, the callback is called immediately!
-				future.onComplete((AsyncResult<T> result2) -> {
-					resultRef.set(result2);
-					synchronized (lock) {
-						lock.notify();
-					}
-				});
-				while (resultRef.get() == null) {
-					// Nested sync on lock is fine.  If we get a spurious wake-up before resultRef is set, we need to reacquire the lock, then wait again.
-					// Ref: https://stackoverflow.com/a/249907/257299
-					synchronized (lock) {
-						// @Blocking
-						lock.wait();
-					}
-				}
-			}
-			AsyncResult<T> asyncResult = resultRef.get();
-			result = asyncResult.result();
-			cause = asyncResult.cause();
-		}
-
-		if (cause != null)
-			throw new RuntimeException("Future failed", cause);
-
-		return result;
-
-	}
-
 	private static final Logger Log = LoggerFactory.getLogger(Dolphin.class);
 
 	private final EventBus eventBus;
-	private final Vertx vertx;
-
-	private final Set<String> verticeDeploymentIds = new HashSet<>();
 
 }
---
+++ dolphin/src/main/java/com/steatoda/muddywaters/dolphin/vertx/VertxService.java
@@ -0,0 +1,142 @@
+package com.steatoda.muddywaters.dolphin.vertx;
+
+import com.steatoda.muddywaters.dolphin.DolphinDestroyEvent;
+import com.steatoda.muddywaters.dolphin.DolphinPreStartEvent;
+import com.steatoda.muddywaters.dolphin.DolphinPreStopEvent;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * <p>Wraps {@link Vertx}, manages its lifecycle and makes sure it is closed on app shutdown.</p>
+ */
+@Singleton
+public class VertxService {
+
+	@Inject
+	public VertxService(Vertx vertx, EventBus eventBus) {
+		this.vertx = vertx;
+		eventBus.register(this);
+	}
+
+	@Subscribe
+	@SuppressWarnings("unused")
+	public void onCityPreStart(DolphinPreStartEvent event) {
+
+		try {
+			awaitComplete(CompositeFuture.all(
+				Stream.of(
+					RestVerticle.deploy(vertx)
+				).peek(future -> future.onSuccess(verticeDeploymentIds::add)).collect(Collectors.toList())
+			));
+		} catch (Exception e) {
+			throw new RuntimeException("Error deploying vertice(s)", e);
+		}
+
+		Log.info("{} Vert.x vertice(s) deployed", verticeDeploymentIds.size());
+
+	}
+
+	@Subscribe
+	@SuppressWarnings("unused")
+	public void onCityPreStop(DolphinPreStopEvent event) {
+
+		int verticlesCount = verticeDeploymentIds.size();
+
+		if (verticlesCount > 0) {
+			try {
+				awaitComplete(CompositeFuture.all(verticeDeploymentIds.stream().map(vertx::undeploy).collect(Collectors.toList())));
+				verticeDeploymentIds.clear();
+			} catch (Exception e) {
+				Log.error("Error undeploying vertices", e);
+			}
+		}
+
+		Log.info("{} vertices undeployed", verticlesCount);
+
+	}
+
+	@Subscribe
+	@SuppressWarnings("unused")
+	public void onCityDestroy(DolphinDestroyEvent event) {
+
+		try {
+			Promise<Void> vertxClosePromise = Promise.promise();
+			vertx.close(result -> {
+				if (result.succeeded())
+					vertxClosePromise.complete(result.result());
+				else
+					vertxClosePromise.fail(result.cause());
+			});
+			awaitComplete(vertxClosePromise.future());
+		} catch (Exception e) {
+			Log.error("Error closing Vert.x", e);
+		}
+
+		Log.info("Vert.x closed");
+
+	}
+
+	// adopted from https://stackoverflow.com/a/68543780/4553548
+	private static <T> T awaitComplete(io.vertx.core.Future<T> future) throws InterruptedException {
+
+		final Object lock = new Object();
+		final AtomicReference<AsyncResult<T>> resultRef = new AtomicReference<>(null);
+
+		T result;
+		Throwable cause;
+
+		if (future.isComplete()) {
+			result = future.result();
+			cause = future.cause();
+		} else {
+			synchronized (lock) {
+				// We *must* be locked before registering a callback.
+				// If result is ready, the callback is called immediately!
+				future.onComplete((AsyncResult<T> result2) -> {
+					resultRef.set(result2);
+					synchronized (lock) {
+						lock.notify();
+					}
+				});
+				while (resultRef.get() == null) {
+					// Nested sync on lock is fine.  If we get a spurious wake-up before resultRef is set, we need to reacquire the lock, then wait again.
+					// Ref: https://stackoverflow.com/a/249907/257299
+					synchronized (lock) {
+						// @Blocking
+						lock.wait();
+					}
+				}
+			}
+			AsyncResult<T> asyncResult = resultRef.get();
+			result = asyncResult.result();
+			cause = asyncResult.cause();
+		}
+
+		if (cause != null)
+			throw new RuntimeException("Future failed", cause);
+
+		return result;
+
+	}
+
+	private static final Logger Log = LoggerFactory.getLogger(VertxService.class);
+
+	private final Vertx vertx;
+
+	private final Set<String> verticeDeploymentIds = new HashSet<>();
+
+}
