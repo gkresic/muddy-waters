4cdcdeda038c7b19e68b60a6a56e944e7cf6fc4d
gkresic <gordan.kresic@steatoda.com>
2023-01-06 16:54:43 +0100

    Sailfish: fix native build

--- sailfish/src/main/java/com/steatoda/muddywaters/sailfish/Payload.java
+++ sailfish/src/main/java/com/steatoda/muddywaters/sailfish/Payload.java
@@ -1,13 +1,20 @@
 package com.steatoda.muddywaters.sailfish;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.micronaut.core.annotation.Introspected;
 
+@Introspected
 public class Payload {
 
 	@JsonProperty
-	public String text;
+	public String getText() { return text; }
+	public void setText(String text) { this.text = text; }
 
 	@JsonProperty
+	public Integer getNumber() { return number; }
+	public void setNumber(Integer number) { this.number = number; }
+
+	public String text;
 	public Integer number;
 
 }
